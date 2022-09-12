aab6f05ff8d1f9b5305bc3f6e5b9b3a47b3df860
hey <blabla>
2022-09-11 19:06:42 -0500

    added changes to the main branch

--- Camelcase.py
+++ Camelcase.py
@@ -1,12 +1,21 @@
-
-
-"""This program is """
-
+def camelcase(sentence):
+    """ Convert sentence to camelCase, for example, 'Display all books'
+  is converted to 'displayAllBooks' """
+    title_case = sentence.title() # Uppercase first letter of each word
+    upper_camel_cased = title_case.replace(' ', '') # remove spaces
+# Lowercase first letter, join with rest of string
+# Slices don't produce index out of bounds errors.
+# So this still works on empty strings, strings of length 1
+    return upper_camel_cased[0:1].lower() + upper_camel_cased[1:]
 
 
 def main():
-    stringtoCamelcase= input('Enter a sentence to convert to camelCase')
-    Camel_casestring=(stringtoCamelcase)
-    print(Camel_casestring)
+ 
+  sentence = input('Enter your sentence: ')
+  output = camelcase(sentence) 
+  print(output)
+if __name__ == '__main__':
+    main()
+
 
     
